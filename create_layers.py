af9e2c0100d9b983c297e29b3fe8793ff2ac3287
Cyril ALFARO <cyril@dbyzero.com>
2015-11-30 22:48:54 +0100

    Initial commit

---
+++ create_layers.py
@@ -0,0 +1,19 @@
+def dbyzero_deimos_layers_creation( img ):
+    pass
+
+register(
+    "dbyzero_deimos_layers_creation",
+    "Create basic layer structure to use with spritesheets",
+    "Create basic layer structure to use with spritesheets",
+    "Dbyzero",
+    "GPL Licence",
+    "2015",
+    "<Image>/Deimos/Create layers structure",
+    "",
+    [],
+    [],
+    dbyzero_deimos_layers_creation,
+    menu="<Image>/Deimos/Create layers structure"
+)
+
+main()
