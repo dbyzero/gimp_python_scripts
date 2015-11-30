d9f8ab8b489a7a448e4ec7f9b390bc0d12776a1e
dbyzero <cyril@dbyzero.com>
2015-12-01 00:50:50 +0100

    Create first group and layer on a new image

--- create_layers.py
+++ create_layers.py
@@ -1,5 +1,19 @@
-def dbyzero_deimos_layers_creation( img ):
-    pass
+#!/usr/bin/env python
+
+from gimpfu import *
+import gimp
+
+def dbyzero_deimos_layers_creation():
+    rawImage = pdb.gimp_image_new( 1000, 1000, RGB )
+    pdb.gimp_display_new(rawImage)
+    image = gimp.image_list()[0]
+    layer = gimp.Layer(image, "calque1", 100, 100, RGB_IMAGE, 100, NORMAL_MODE)
+    layer.translate(100,100)
+
+    group_layer = pdb.gimp_layer_group_new(image)
+    pdb.gimp_image_insert_layer(image, group_layer, None, 1)
+    pdb.gimp_image_insert_layer(image, layer, group_layer, 1)
+
 
 register(
     "dbyzero_deimos_layers_creation",
@@ -8,12 +22,12 @@ register(
     "Dbyzero",
     "GPL Licence",
     "2015",
-    "<Image>/Deimos/Create layers structure",
-    "",
+    "Create deimos layers structure",
+    "*",
     [],
     [],
     dbyzero_deimos_layers_creation,
-    menu="<Image>/Deimos/Create layers structure"
+    menu="<Image>/Deimos"
 )
 
 main()
